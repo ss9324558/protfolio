fd223370e222f3b1eb9ce0c6c69414a610ba06e6
ss9324558 <115810209+ss9324558@users.noreply.github.com>
2023-04-09 03:00:49 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Typing Animation
+var typed=new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Programmer","Fresher"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
